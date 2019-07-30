3323bcd08bf96a578fc424667fc174a055ae8252
ming <mkl0819@naver.com>
2019-07-30 10:19:22 +0900

    [MOD] CHATBOT COMPONENT

---
+++ src/components/ChatBotBtn.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-layout>
+    <v-tooltip bottom>
+      <template v-slot:activator="{ on }">
+        <v-img
+          id="chat-icon"
+          v-on="on"
+          :src="require('@/assets/korea.png')"
+          width="30px"
+          height="30px"
+          style="margin:auto; margin-right: 10px;"
+          @click="chatOpen()"
+        ></v-img>
+      </template>
+      <span>
+        버튼을 눌러 주인장과 대화해보세요!
+        <v-icon small color="#FDD835">far fa-grin-squint</v-icon>
+      </span>
+    </v-tooltip>
+    <ChatBot  />
+  </v-layout>
+  <!-- data-init-식별키=값 으로 셋팅하면 챗플로우에 파라미터와 연동가능. 식별키는 소문자만 가능 -->
+</template>
+
+<script>
+import { log } from "util";
+import ChatBot from "../components/Chatbot.vue";
+export default {
+  components: {
+    ChatBot
+  }
+};
+</script>
+
+
