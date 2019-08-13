5e74811ad73a00ace7af6400385b69f933e8b200
Lee Sang Ju <leesangju92@gmail.com>
2019-08-13 16:46:03 +0900

    fix3

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -307,10 +307,10 @@
     .grow:hover { transform: scale(1.02); }
     .slideButton {
         font-size: 3em;
-        color: rgba(50, 50, 50, 0.2);
+        color: rgba(50, 50, 50, 0.6);
     }
     .slideButton:hover {
-        color: rgba(255,250,240, 0.8)
+        color: rgba(255,250,240, 0.6)
     }
 
     .greyBackground {
